822c794ba581d600043b5c7c3dc8e54e1b525186
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-08-03 09:24:06 +0200

    CrossProcess reader counter.

---
+++ CrossProcessReaderWriterLock/Threading.CrossProcess/ReaderCounter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading.CrossProcess
+{
+    /// <summary>
+    /// Counter to track the number of processes or threads that can read access to the repository.
+    /// </summary>
+    internal class ReaderCounter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InterProcessReadCounter"/> class.
+        /// </summary>
+        /// <param name="maxConcurrentRead">The maximum number of reader requests that can be granted concurrently.</param>
+        internal ReaderCounter(int maxConcurrentRead)
+        {
+            myMaximumCount = maxConcurrentRead + InitialCount;
+            myReadCounterSemaphore = new Semaphore(InitialCount, myMaximumCount, "Counter");
+            myIncomingOperation = new Semaphore(1, 1, "Incoming");
+        }
+
+        /// <summary>
+        /// Increases the read counter by 1 across processes.
+        /// </summary>
+        /// <returns></returns>
+        internal int Increase()
+        {
+            // Make sure for atomic increase
+            myIncomingOperation.WaitOne();
+
+            int counter = RetrieveCurrentCount();
+
+            // Not allowing to exceed maximum count
+            if (counter != myMaximumCount - 1)
+            {
+                counter = myReadCounterSemaphore.Release();
+            }
+            else
+            {
+                counter++;
+            }
+
+            myIncomingOperation.Release();
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Decreases the read counter by 1 across processes.
+        /// </summary>
+        /// <returns></returns>
+        internal int Decrease()
+        {
+            // Make sure for atomic decrease
+            myIncomingOperation.WaitOne();
+
+            int counter = RetrieveCurrentCount() - 1;
+            myReadCounterSemaphore.WaitOne(0);
+
+            myIncomingOperation.Release();
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance.
+        /// </summary>
+        public void Dispose()
+        {
+            myReadCounterSemaphore?.Dispose();
+            myIncomingOperation?.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Maximum number of concurrent read count.
+        /// </summary>
+        private readonly int myMaximumCount;
+
+        /// <summary>
+        /// Initial count in order to safely exceed or deceed semaphore.
+        /// </summary>
+        private const int InitialCount = 1;
+
+        /// <summary>
+        /// Semaphore to keep track of number of reader.
+        /// </summary>
+        private readonly Semaphore myReadCounterSemaphore;
+
+        /// <summary>
+        /// Semaphore to ensure atomic increase and decrease.
+        /// </summary>
+        private readonly Semaphore myIncomingOperation;
+
+        /// <summary>
+        /// Retrieves the current number of readers.
+        /// </summary>
+        /// <returns>The active reader counts.</returns>
+        private int RetrieveCurrentCount()
+        {
+            myReadCounterSemaphore.WaitOne(0);
+            int counter = myReadCounterSemaphore.Release();
+            return counter;
+        }
+    }
+}
